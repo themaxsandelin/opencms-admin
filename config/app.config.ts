bdd19f46fd5cd790fcdd7de35272b8cd05031629
Max Sandelin <themaxsandelin@gmail.com>
2022-12-06 09:12:39 +0100

    Wrong service name.

--- config/app.config.ts
+++ config/app.config.ts
@@ -1,9 +1,9 @@
 const hosts: { [key: string]: string } = {
   local: 'http://localhost:3300',
-  development: 'http://opencms-mirror-admin-ui',
-  staging: 'http://opencms-mirror-admin-ui',
-  preprod: 'http://opencms-mirror-admin-ui',
-  production: 'http://opencms-mirror-admin-ui',
+  development: 'http://opencms-admin-mirror-admin-ui',
+  staging: 'http://opencms-admin-mirror-admin-ui',
+  preprod: 'http://opencms-admin-mirror-admin-ui',
+  production: 'http://opencms-admin-mirror-admin-ui',
 };
 
 export default hosts;
