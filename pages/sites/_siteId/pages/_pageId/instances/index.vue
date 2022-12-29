09e4682e987eda4a3789d54811ecaa06e674e919
Linus Brimstedt <linus.brimstedt@ellos.se>
2022-12-29 21:36:05 +0100

    cs: more whitespace

--- pages/sites/_siteId/pages/_pageId/instances/index.vue
+++ pages/sites/_siteId/pages/_pageId/instances/index.vue
@@ -50,97 +50,97 @@
 
 <script>
 // Components
-import PageTabs from '@/components/molecules/page-tabs';
-import InstanceForm from '@/components/organisms/instance-form';
+  import PageTabs from '@/components/molecules/page-tabs';
+  import InstanceForm from '@/components/organisms/instance-form';
 
-export default {
-  name: 'PageInstances',
-  components: {
-    PageTabs,
-    InstanceForm
-  },
-  data() {
-    return {
-      search: '',
-      instances: [],
-      headers: [
-        {
-          text: 'Title',
-          value: 'title',
-          align: 'start'
-        },
-        {
-          text: 'Slug',
-          value: 'slug'
-        },
-        {
-          text: 'Path',
-          value: 'path'
-        },
-        {
-          text: 'Locale',
-          value: 'localeCode'
-        },
-        {
-          text: 'Updated',
-          value: 'updatedAt'
-        },
-        {
-          text: 'Updated by',
-          value: 'updatedBy'
-        },
-        {
-          text: 'Created',
-          value: 'createdAt'
-        },
-        {
-          text: 'Created by',
-          value: 'createdBy'
-        },
-        {
-          text: '',
-          value: 'actions'
-        }
-      ],
-      instanceFormVisible: false,
-      editingInstance: null
-    };
-  },
-  async fetch() {
-    const { siteId, pageId } = this.$route.params;
-    const { data, error } = await this.$api(`/sites/${siteId}/pages/${pageId}/instances`);
-    if (error) {
-      console.error(error);
-      return this.$store.commit('alert/set', { type: 'error', message: error });
+  export default {
+    name: 'PageInstances',
+    components: {
+      PageTabs,
+      InstanceForm
+    },
+    data() {
+      return {
+        search: '',
+        instances: [],
+        headers: [
+          {
+            text: 'Title',
+            value: 'title',
+            align: 'start'
+          },
+          {
+            text: 'Slug',
+            value: 'slug'
+          },
+          {
+            text: 'Path',
+            value: 'path'
+          },
+          {
+            text: 'Locale',
+            value: 'localeCode'
+          },
+          {
+            text: 'Updated',
+            value: 'updatedAt'
+          },
+          {
+            text: 'Updated by',
+            value: 'updatedBy'
+          },
+          {
+            text: 'Created',
+            value: 'createdAt'
+          },
+          {
+            text: 'Created by',
+            value: 'createdBy'
+          },
+          {
+            text: '',
+            value: 'actions'
+          }
+        ],
+        instanceFormVisible: false,
+        editingInstance: null
+      };
+    },
+    async fetch() {
+      const { siteId, pageId } = this.$route.params;
+      const { data, error } = await this.$api(`/sites/${siteId}/pages/${pageId}/instances`);
+      if (error) {
+        console.error(error);
+        return this.$store.commit('alert/set', { type: 'error', message: error });
+      }
+      this.$set(this.$data, 'instances', data);
+    },
+    methods: {
+      instanceRowClick(instance) {
+        this.$router.push(`${this.$route.path}/${instance.id}`);
+      },
+      showInstanceForm() {
+        this.instanceFormVisible = true;
+      },
+      hideInstanceForm() {
+        this.instanceFormVisible = false;
+      },
+      createdCallback() {
+        this.$fetch();
+      },
+      updatedCallback() {
+        this.$set(this.$data, 'editingInstance', null);
+        this.$fetch();
+      },
+      deletedCallback() {
+        this.$set(this.$data, 'editingInstance', null);
+        this.$fetch();
+      },
+      editInstance(event, instance) {
+        event.stopPropagation();
+        this.$set(this.$data, 'editingInstance', instance);
+        this.showInstanceForm();
+      }
     }
-    this.$set(this.$data, 'instances', data);
-  },
-  methods: {
-    instanceRowClick(instance) {
-      this.$router.push(`${this.$route.path}/${instance.id}`);
-    },
-    showInstanceForm() {
-      this.instanceFormVisible = true;
-    },
-    hideInstanceForm() {
-      this.instanceFormVisible = false;
-    },
-    createdCallback() {
-      this.$fetch();
-    },
-    updatedCallback() {
-      this.$set(this.$data, 'editingInstance', null);
-      this.$fetch();
-    },
-    deletedCallback() {
-      this.$set(this.$data, 'editingInstance', null);
-      this.$fetch();
-    },
-    editInstance(event, instance) {
-      event.stopPropagation();
-      this.$set(this.$data, 'editingInstance', instance);
-      this.showInstanceForm();
-    }
-  }
-};
+  };
 </script>
