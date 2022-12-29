b87bc3118b6d9024ca6cb62896e081b8b83f6f20
Linus Brimstedt <linus.brimstedt@ellos.se>
2022-12-29 21:32:59 +0100

    cs: more whitespace

--- pages/sites/_siteId/pages/_pageId/instances/_instanceId/index.vue
+++ pages/sites/_siteId/pages/_pageId/instances/_instanceId/index.vue
@@ -38,184 +38,184 @@
 
 <script>
 // Components
-import PageTabs from '@/components/molecules/page-tabs';
+  import PageTabs from '@/components/molecules/page-tabs';
 
-export default {
-  name: 'InstancePage',
-  components: {
-    PageTabs
-  },
-  data() {
-    return {
-      instance: {},
-      publishingEnvironments: [],
-      layouts: [],
-      layoutEnvironments: {},
-      instanceChanges: {
-        data: {
-          source: undefined,
-          type: undefined
+  export default {
+    name: 'InstancePage',
+    components: {
+      PageTabs
+    },
+    data() {
+      return {
+        instance: {},
+        publishingEnvironments: [],
+        layouts: [],
+        layoutEnvironments: {},
+        instanceChanges: {
+          data: {
+            source: undefined,
+            type: undefined
+          },
+          environments: {}
         },
-        environments: {}
+        dataOptions: {
+          sources: [
+            {
+              text: 'Route parameter',
+              value: 'route-parameter'
+            }
+          ],
+          types: [
+            {
+              text: 'Question category (Content Type)',
+              value: 'content-block_question-category'
+            },
+            {
+              text: 'Question (Content Type)',
+              value: 'content-block_question'
+            }
+          ]
+        }
+      };
+    },
+    async fetch() {
+      if (!this.instance.id) {
+        await this.updateInstance();
+        await this.updateInstanceLayouts();
+      }
+      if (!this.publishingEnvironments.length) {
+        await this.updatePublishingEnvironments();
+      }
+      if (!this.layouts.length) {
+        await this.updateLayouts();
+      }
+    },
+    computed: {
+      layoutList() {
+        return this.layouts.map((layout) => ({
+          text: layout.name,
+          value: layout.id
+        }));
       },
-      dataOptions: {
-        sources: [
-          {
-            text: 'Route parameter',
-            value: 'route-parameter'
-          }
-        ],
-        types: [
-          {
-            text: 'Question category (Content Type)',
-            value: 'content-block_question-category'
-          },
-          {
-            text: 'Question (Content Type)',
-            value: 'content-block_question'
-          }
-        ]
-      }
-    };
-  },
-  async fetch() {
-    if (!this.instance.id) {
-      await this.updateInstance();
-      await this.updateInstanceLayouts();
-    }
-    if (!this.publishingEnvironments.length) {
-      await this.updatePublishingEnvironments();
-    }
-    if (!this.layouts.length) {
-      await this.updateLayouts();
-    }
-  },
-  computed: {
-    layoutList() {
-      return this.layouts.map((layout) => ({
-        text: layout.name,
-        value: layout.id
-      }));
-    },
-    instanceData() {
-      return this.instance.config && this.instance.config.data ? this.instance.config.data : {};
-    },
-    changesMade() {
-      return (
-        Object.keys(this.instanceChanges.environments).length > 0 ||
-        (this.instanceChanges.data.source !== this.instanceData.source && this.instanceChanges.data.type !== this.instanceData.type)
-      );
-    }
-  },
-  methods: {
-    async updateInstance() {
-      const { siteId, pageId, instanceId } = this.$route.params;
-      const { data, error } = await this.$api(`/sites/${siteId}/pages/${pageId}/instances/${instanceId}`);
-      if (error) {
-        console.error(error);
-        return this.$store.commit('alert/set', {
-          type: 'error',
-          message: error
-        });
-      }
-      this.$set(this.$data, 'instance', data);
-      if (data.config && data.config.data) {
-        this.$set(this.$data.instanceChanges, 'data', data.config.data);
-      }
-    },
-    async updateInstanceLayouts() {
-      const { siteId, pageId, instanceId } = this.$route.params;
-      const { data, error } = await this.$api(`/sites/${siteId}/pages/${pageId}/instances/${instanceId}/layouts`);
-      if (error) {
-        console.error(error);
-        return this.$store.commit('alert/set', {
-          type: 'error',
-          message: error
-        });
-      }
-      const layoutEnvironments = {};
-      data.forEach((instanceLayout) => {
-        layoutEnvironments[instanceLayout.publishingEnvironmentId] = instanceLayout.pageLayoutId;
-      });
-      this.$set(this.$data, 'layoutEnvironments', layoutEnvironments);
-    },
-    async updatePublishingEnvironments() {
-      const { data, error } = await this.$api('/publishing-environments');
-      if (error) {
-        console.error(error);
-        return this.$store.commit('alert/set', {
-          type: 'error',
-          message: error
-        });
-      }
-      this.$set(this.$data, 'publishingEnvironments', data);
-    },
-    async updateLayouts() {
-      const { siteId, pageId } = this.$route.params;
-      const { data, error } = await this.$api(`/sites/${siteId}/pages/${pageId}/layouts`);
-      if (error) {
-        console.error(error);
-        return this.$store.commit('alert/set', {
-          type: 'error',
-          message: error
-        });
-      }
-      this.$set(this.$data, 'layouts', data);
-    },
-    layoutChange(layoutId, environmentKey) {
-      const changes = { ...this.instanceChanges };
-      const selection = { ...this.layoutEnvironments };
-      changes.environments[environmentKey] = layoutId;
-
-      this.$set(this.$data, 'instanceChanges', changes);
-      this.$set(this.$data, 'layoutEnvironments', selection);
-    },
-    async save() {
-      const { data } = this.instanceChanges;
-      const environmentIds = Object.keys(this.instanceChanges.environments);
-      const { siteId, pageId, instanceId } = this.$route.params;
-
-      try {
-        await Promise.all(
-          environmentIds.map((publishingEnvironmentId) => {
-            const body = {
-              publishingEnvironmentId,
-              layoutId: this.instanceChanges.environments[publishingEnvironmentId]
-            };
-            return this.$api(`/sites/${siteId}/pages/${pageId}/instances/${instanceId}/layouts`, {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify(body)
-            });
-          })
+      instanceData() {
+        return this.instance.config && this.instance.config.data ? this.instance.config.data : {};
+      },
+      changesMade() {
+        return (
+          Object.keys(this.instanceChanges.environments).length > 0 ||
+          (this.instanceChanges.data.source !== this.instanceData.source && this.instanceChanges.data.type !== this.instanceData.type)
         );
+      }
+    },
+    methods: {
+      async updateInstance() {
+        const { siteId, pageId, instanceId } = this.$route.params;
+        const { data, error } = await this.$api(`/sites/${siteId}/pages/${pageId}/instances/${instanceId}`);
+        if (error) {
+          console.error(error);
+          return this.$store.commit('alert/set', {
+            type: 'error',
+            message: error
+          });
+        }
+        this.$set(this.$data, 'instance', data);
+        if (data.config && data.config.data) {
+          this.$set(this.$data.instanceChanges, 'data', data.config.data);
+        }
+      },
+      async updateInstanceLayouts() {
+        const { siteId, pageId, instanceId } = this.$route.params;
+        const { data, error } = await this.$api(`/sites/${siteId}/pages/${pageId}/instances/${instanceId}/layouts`);
+        if (error) {
+          console.error(error);
+          return this.$store.commit('alert/set', {
+            type: 'error',
+            message: error
+          });
+        }
+        const layoutEnvironments = {};
+        data.forEach((instanceLayout) => {
+          layoutEnvironments[instanceLayout.publishingEnvironmentId] = instanceLayout.pageLayoutId;
+        });
+        this.$set(this.$data, 'layoutEnvironments', layoutEnvironments);
+      },
+      async updatePublishingEnvironments() {
+        const { data, error } = await this.$api('/publishing-environments');
+        if (error) {
+          console.error(error);
+          return this.$store.commit('alert/set', {
+            type: 'error',
+            message: error
+          });
+        }
+        this.$set(this.$data, 'publishingEnvironments', data);
+      },
+      async updateLayouts() {
+        const { siteId, pageId } = this.$route.params;
+        const { data, error } = await this.$api(`/sites/${siteId}/pages/${pageId}/layouts`);
+        if (error) {
+          console.error(error);
+          return this.$store.commit('alert/set', {
+            type: 'error',
+            message: error
+          });
+        }
+        this.$set(this.$data, 'layouts', data);
+      },
+      layoutChange(layoutId, environmentKey) {
+        const changes = { ...this.instanceChanges };
+        const selection = { ...this.layoutEnvironments };
+        changes.environments[environmentKey] = layoutId;
 
-        await this.$api(`/sites/${siteId}/pages/${pageId}/instances/${instanceId}`, {
-          method: 'PATCH',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            config: { data }
-          })
-        });
+        this.$set(this.$data, 'instanceChanges', changes);
+        this.$set(this.$data, 'layoutEnvironments', selection);
+      },
+      async save() {
+        const { data } = this.instanceChanges;
+        const environmentIds = Object.keys(this.instanceChanges.environments);
+        const { siteId, pageId, instanceId } = this.$route.params;
 
-        this.updateInstance();
-        this.updateInstanceLayouts();
-        this.$store.commit('alert/set', {
-          type: 'success',
-          message: 'Instance layout updated!'
-        });
-      } catch (error) {
-        console.error(error);
-        return this.$store.commit('alert/set', {
-          type: 'error',
-          message: error
-        });
+        try {
+          await Promise.all(
+            environmentIds.map((publishingEnvironmentId) => {
+              const body = {
+                publishingEnvironmentId,
+                layoutId: this.instanceChanges.environments[publishingEnvironmentId]
+              };
+              return this.$api(`/sites/${siteId}/pages/${pageId}/instances/${instanceId}/layouts`, {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(body)
+              });
+            })
+          );
+
+          await this.$api(`/sites/${siteId}/pages/${pageId}/instances/${instanceId}`, {
+            method: 'PATCH',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              config: { data }
+            })
+          });
+
+          this.updateInstance();
+          this.updateInstanceLayouts();
+          this.$store.commit('alert/set', {
+            type: 'success',
+            message: 'Instance layout updated!'
+          });
+        } catch (error) {
+          console.error(error);
+          return this.$store.commit('alert/set', {
+            type: 'error',
+            message: error
+          });
+        }
       }
     }
-  }
-};
+  };
 </script>
