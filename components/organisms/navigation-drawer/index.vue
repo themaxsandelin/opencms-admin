5dd538ffed4face867a629a02f14ac2b2701122a
Linus Brimstedt <linus.brimstedt@ellos.se>
2022-12-29 20:03:09 +0100

    fix: Reorganized meny and added icons to all items

--- components/organisms/navigation-drawer/index.vue
+++ components/organisms/navigation-drawer/index.vue
@@ -1,94 +1,110 @@
 <template>
   <v-navigation-drawer permanent fixed app :clipped="true">
+    <v-subheader>Pages</v-subheader>
     <v-list>
-      <v-subheader>Sites</v-subheader>
-      <v-list-item :link="true" to="/sites" :exact="true">All sites</v-list-item>
-
-      <v-list-group
+      <v-list-item
         v-for="(site, i) in sites"
         :key="i"
         :value="$route.path.includes(`/sites/${site.id}`)"
+        :link="true"
+        :to="`/sites/${site.id}/pages`"
       >
-        <template v-slot:activator>
-          <v-list-item-title>{{ site.name }}</v-list-item-title>
-        </template>
-
-        <v-list-item
-          :link="true"
-          :to="`/sites/${site.id}/pages`"
-        >
-          <v-list-item-icon>
-            <v-icon size="20" v-text="`mdi-file-document-multiple`"></v-icon>
-          </v-list-item-icon>
-          <v-list-item-title v-text="`Pages`"></v-list-item-title>
-        </v-list-item>
-      </v-list-group>
-    </v-list>
-
-    <v-list>
-      <v-list-item :link="true" to="/environments" :exact="true">Publishing environments</v-list-item>
-    </v-list>
-
-    <v-list>
-      <v-list-item :link="true" to="/locales">
-        <v-list-item-title>Locales</v-list-item-title>
+        <v-list-item-icon>
+          <v-icon size="20" v-text="`mdi-file-document-multiple`"></v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>{{ site.name }}</v-list-item-title>
       </v-list-item>
     </v-list>
 
+    <v-subheader>Content</v-subheader>
     <v-list>
-      <v-list-item :link="true" to="/logs">
-        <v-list-item-title>Activity logs</v-list-item-title>
+      <v-list-item :link="true" to="/qa/categories">
+        <v-list-item-icon>
+          <v-icon>mdi-shape</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Categories</v-list-item-title>
       </v-list-item>
-    </v-list>
 
-    <v-list>
+      <v-list-item :link="true" to="/qa/questions">
+        <v-list-item-icon>
+          <v-icon>mdi-frequently-asked-questions</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Questions</v-list-item-title>
+      </v-list-item>
+
       <v-list-item :link="true" to="/forms">
+        <v-list-item-icon>
+          <v-icon>mdi-form-textbox</v-icon>
+        </v-list-item-icon>
         <v-list-item-title>Forms</v-list-item-title>
       </v-list-item>
     </v-list>
 
+    <v-divider></v-divider>
+    <v-subheader>Configuration</v-subheader>
+    <v-list-item :link="true" to="/sites" :exact="true">
+      <v-list-item-icon>
+        <v-icon>mdi-web</v-icon>
+      </v-list-item-icon>
+      <v-list-item-title>Sites</v-list-item-title>
+    </v-list-item>
+
     <v-list>
-      <v-subheader>Content blocks</v-subheader>
+      <v-list-item :link="true" to="/environments" :exact="true">
+        <v-list-item-icon>
+          <v-icon>mdi-cube-scan</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Publishing environments</v-list-item-title>
+      </v-list-item>
+    </v-list>
 
-      <v-list-group :value="$route.path.includes('/qa')">
-        <template v-slot:activator>
-          <v-list-item-title>Questions and answers</v-list-item-title>
-        </template>
+    <v-list>
+      <v-list-item :link="true" to="/locales">
+        <v-list-item-icon>
+          <v-icon>mdi-flag-variant-outline</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Locales</v-list-item-title>
+      </v-list-item>
+    </v-list>
 
-        <v-list-item :link="true" to="/qa/categories">
-          <v-list-item-title>Categories</v-list-item-title>
-        </v-list-item>
-
-        <v-list-item :link="true" to="/qa/questions">
-          <v-list-item-title>Questions</v-list-item-title>
-        </v-list-item>
-      </v-list-group>
+    <v-divider></v-divider>
+    <v-subheader>Admin</v-subheader>
+    <v-list>
+      <v-list-item :link="true" to="/logs">
+        <v-list-item-icon>
+          <v-icon>mdi-table-question</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Activity logs</v-list-item-title>
+      </v-list-item>
     </v-list>
   </v-navigation-drawer>
 </template>
 
 <script>
-  export default {
-    name: 'NavigationDrawer',
-    data() {
-      return {
-        sites: []
-      };
-    },
-    async fetch() {
-      const { data, error } = await this.$api('/sites');
-      if (error) {
-        console.error(error);
-        return this.$store.commit('alert/set', { message: 'Failed to load sites.', type: 'error' });
-      }
-      this.$set(this.$data, 'sites', data);
-    },
-    methods: {
-      clickSite() {}
+export default {
+  name: 'NavigationDrawer',
+  data() {
+    return {
+      sites: [],
     }
-  }
+  },
+  async fetch() {
+    const { data, error } = await this.$api('/sites')
+    if (error) {
+      console.error(error)
+      return this.$store.commit('alert/set', {
+        message: 'Failed to load sites.',
+        type: 'error',
+      })
+    }
+    this.$set(this.$data, 'sites', data)
+  },
+  methods: {
+    clickSite() {},
+  },
+}
 </script>
 
 <style lang="scss" scoped>
-  @import './style';
+@import './style';
 </style>
