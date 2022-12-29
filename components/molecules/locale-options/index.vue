5b04471dd9a38caceccdaab7d6e5520a963dd008
Linus Brimstedt <linus.brimstedt@ellos.se>
2022-12-29 21:08:37 +0100

    fix: Changed format and order of locales

--- components/molecules/locale-options/index.vue
+++ components/molecules/locale-options/index.vue
@@ -3,41 +3,41 @@
 </template>
 
 <script>
-  export default {
-    name: 'LocaleOptions',
-    props: {
-      code: {
-        type: String,
-        default: ''
-      }
-    },
-    data() {
-      return {
-        options: []
-      };
-    },
-    async fetch() {
-      const { data, error } = await this.$api('/locales/options');
-      if (error) {
-        console.error('Failed to fetch locales options', error);
-        return this.$store.commit('alert/set', { type: 'error', message: error });
-      }
-
-      this.$set(this.$data, 'options', data);
-    },
-    computed: {
-      localeOptions() {
-        return this.options.map(locale => ({
-          text: `${locale.code} - ${locale.name}`,
-          value: locale.code
-        }));
-      }
-    },
-    methods: {
-      localeSelection(code) {
-        const locale = this.options.find(option => option.code === code);
-        this.$emit('selection', locale);
-      }
+export default {
+  name: 'LocaleOptions',
+  props: {
+    code: {
+      type: String,
+      default: ''
     }
-  };
+  },
+  data() {
+    return {
+      options: []
+    };
+  },
+  async fetch() {
+    const { data, error } = await this.$api('/locales/options');
+    if (error) {
+      console.error('Failed to fetch locales options', error);
+      return this.$store.commit('alert/set', { type: 'error', message: error });
+    }
+
+    this.$set(this.$data, 'options', data);
+  },
+  computed: {
+    localeOptions() {
+      return this.options.map((locale) => ({
+        text: `${locale.name} (${locale.code})`,
+        value: locale.code
+      }));
+    }
+  },
+  methods: {
+    localeSelection(code) {
+      const locale = this.options.find((option) => option.code === code);
+      this.$emit('selection', locale);
+    }
+  }
+};
 </script>
