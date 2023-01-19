0350c63c34104560b05d9081c83343a3c8ce2544
Linus Brimstedt <linus.brimstedt@ellos.se>
2023-01-20 00:37:46 +0100

    feat: show flags to indicate existing translations feat: use router-link for better jumping between pages

---
+++ components/molecules/locale-icon/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <img v-id="iconName" :src="iconName" />
+</template>
+
+<script>
+  export default {
+    name: 'LocaleIcon',
+    props: {
+      locale: {
+        type: String,
+        default: ''
+      }
+    },
+    computed: {
+      iconName() {
+        if (!this.locale) {
+          return '';
+        }
+
+        const parts = this.locale.toLowerCase().split('-');
+        if (parts.length > 1) {
+          return `/flags/${parts[1]}.svg`;
+        }
+        return '';
+      }
+    }
+  };
+</script>
+<style scoped="true">
+img {
+  width: 1.2em;
+  margin: 0 0.1em;
+  border: 1px solid #ccc;
+}
+</style>
